b4ef4b0ea966549fadd3c5def7ef14bacee7dc68
SomaCruz0 <le933333@gmail.com>
2024-06-29 16:32:42 +0700

    v#

---
+++ WebBanGiay.Module/BusinessObjects/ORMDataModel1Code/KhachHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace WebBanGiay.Module.BusinessObjects.ORMDataModel1
+{
+    [DefaultClassOptions]
+    public partial class KhachHang
+    {
+        public KhachHang(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+
+        public KhachHang() { }
+    }
+
+}
